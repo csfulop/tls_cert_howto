489a2267c6b8c05facc49753445dbc8a453a5ef0
Csaba Fülöp <csfulop@users.noreply.github.com>
2020-04-18 11:10:08 +0200

    generate CA and server keys and certs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Test Driven Learning: TLS
+
+Based on:
+* https://dev.to/techschoolguru/a-complete-overview-of-ssl-tls-and-its-cryptographic-system-36pd
+* https://dev.to/techschoolguru/how-to-create-sign-ssl-tls-certificates-2aai
+
+
+
---
+++ cert.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+OUT=out
+
+
+function init() {
+  rm -rf $OUT
+  mkdir $OUT
+}
+
+
+function generateCA() {
+  # generate self signed cert (with public key) and private key
+  openssl req -x509 -newkey rsa:4096 -days 365 -nodes \
+    -keyout $OUT/ca-key.pem \
+    -out $OUT/ca-cert.pem \
+    -subj "/C=HU/L=Leanyfalu/O=FCs CA Co./CN=ca.fcs.hu"
+
+  # output the CA cert in text format
+  openssl x509 -in $OUT/ca-cert.pem -noout -text
+}
+
+
+function generateServerCert() {
+  # generate server CSR (Certificate Signing Request)
+  openssl req -newkey rsa:4096 -nodes \
+    -keyout $OUT/server-key.pem \
+    -out $OUT/server-req.pem \
+    -subj "/C=HU/L=Leanyfalu/O=FCs Server Co./CN=server.fcs.hu"
+
+  # sign the CSR with the CA private key
+  openssl x509 -req -days 365 \
+    -in $OUT/server-req.pem \
+    -CA $OUT/ca-cert.pem \
+    -CAkey $OUT/ca-key.pem \
+    -CAcreateserial \
+    -out $OUT/server-cert.pem \
+    -extfile server-ext.cnf
+
+  # output the server cert in text format
+  openssl x509 -in $OUT/server-cert.pem -noout -text
+}
+
+
+function main() {
+  init
+  generateCA
+  generateServerCert
+}
+
+main
