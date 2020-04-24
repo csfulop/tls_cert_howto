0ae8a1605ea11a7fc4edcea3e5672b7518edb63e
Csaba Fülöp <csfulop@users.noreply.github.com>
2020-04-24 14:19:48 +0200

    use Intermediate CA

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ Based on:
 * https://dev.to/techschoolguru/a-complete-overview-of-ssl-tls-and-its-cryptographic-system-36pd
 * https://dev.to/techschoolguru/how-to-create-sign-ssl-tls-certificates-2aai
 
+To work with Intermediate CA:
+* https://github.com/stanzgy/wiki/blob/master/network/openssl-self-signed-certs-cheatsheet.md
+* https://medium.com/@superseb/get-your-certificate-chain-right-4b117a9c0fce
 
-
+More detailed guides:
+* https://medium.com/@brunoosiek/ssl-tls-certificate-authority-ca-with-a-wrinkle-4b43a881edd0
+* https://jamielinux.com/docs/openssl-certificate-authority/
--- cert.sh
+++ cert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 OUT=out
 
@@ -9,7 +9,7 @@ function init() {
 }
 
 
-function generateCA() {
+function generateCACert() {
   # generate self signed cert (with public key) and private key
   openssl req -x509 -newkey rsa:4096 -days 365 -nodes \
     -keyout $OUT/ca-key.pem \
@@ -43,17 +43,90 @@ function generateServerCert() {
 
 
 function verifyServerCert() {
+  # issuer of the cert
+  openssl x509 -in $OUT/server-cert.pem -noout -issuer
+
+  # subject of the CA cert
+  openssl x509 -in $OUT/ca-cert.pem -noout -subject
+
   openssl verify \
     -CAfile $OUT/ca-cert.pem \
     $OUT/server-cert.pem
 }
 
 
-function main() {
-  init
-  generateCA
-  generateServerCert
-  verifyServerCert
+function generateIntermediateCACert() {
+  # generate Intermediate CA CSR (Certificate Signing Request)
+  openssl req -newkey rsa:4096 -nodes \
+    -extensions v3_ca -reqexts v3_req \
+    -keyout $OUT/intermediateca-key.pem \
+    -out $OUT/intermediateca-req.pem \
+    -subj "/C=HU/L=Leanyfalu/O=FCs IntermediateCA Co./CN=intermediateca.fcs.hu"
+
+  # sign the CSR with the CA private key
+  openssl x509 -req -days 365 \
+    -extfile v3_intermediateca_ext.cnf \
+    -in $OUT/intermediateca-req.pem \
+    -CA $OUT/ca-cert.pem \
+    -CAkey $OUT/ca-key.pem \
+    -CAcreateserial \
+    -out $OUT/intermediateca-cert.pem
+
+  # output the Intermediate CA cert in text format
+  openssl x509 -in $OUT/intermediateca-cert.pem -noout -text
 }
 
-main
+
+function generateServerCertWithIntermediateCA() {
+  # generate server CSR (Certificate Signing Request)
+  openssl req -newkey rsa:4096 -nodes \
+    -keyout $OUT/server2-key.pem \
+    -out $OUT/server2-req.pem \
+    -subj "/C=HU/L=Leanyfalu/O=FCs Server With IntermediateCA Co./CN=server2.fcs.hu"
+
+  # sign the CSR with the CA private key
+  openssl x509 -req -days 365 \
+    -in $OUT/server2-req.pem \
+    -CA $OUT/intermediateca-cert.pem \
+    -CAkey $OUT/intermediateca-key.pem \
+    -CAcreateserial \
+    -out $OUT/server2-cert.pem \
+    -extfile server2-ext.cnf
+
+  # output the server cert in text format
+  openssl x509 -in $OUT/server2-cert.pem -noout -text
+}
+
+
+function verifyServerCertWithIntermediateCA() {
+  # issuer of the cert
+  openssl x509 -in $OUT/server2-cert.pem -noout -issuer
+
+  # subject and issuer of the IntermediateCA cert
+  openssl x509 -in $OUT/intermediateca-cert.pem -noout -subject -issuer
+
+  # subject of the CA cert
+  openssl x509 -in $OUT/ca-cert.pem -noout -subject
+
+  openssl verify \
+    -CAfile $OUT/ca-cert.pem \
+    -untrusted $OUT/intermediateca-cert.pem \
+    $OUT/server2-cert.pem
+}
+
+
+function main() {
+  init
+
+  generateCACert
+
+  generateServerCert
+  verifyServerCert
+
+  generateIntermediateCACert
+
+  generateServerCertWithIntermediateCA
+  verifyServerCertWithIntermediateCA
+}
+
+main |& tee log.txt
